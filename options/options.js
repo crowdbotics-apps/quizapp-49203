00c17b82243b6fec71a55b46e474b50eb607b73e
Vadym Kolpak <vadym@tuluko.eu>
2024-08-30 20:24:58 +0300

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "quizapp-49203",
-  url: "https://quizapp-49203.botics.co",
-  api: "https://quizapp-49203.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "quizapp-49203",
+  "url": "https://quizapp-49203.botics.co",
+  "api": "https://quizapp-49203.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "NameAndYear"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
